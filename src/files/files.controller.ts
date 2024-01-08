dbcf68623a393d8118c81ce7aefa2c372681ecc8
Andres Vega <avega@cdt.com.ar>
2024-01-07 22:02:51 -0300

    Validando que el archivo sea una imagen

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -3,17 +3,26 @@ import {
   Post,
   UseInterceptors,
   UploadedFile,
+  BadRequestException,
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter.helper';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+    }),
+  )
   uploadProductFile(@UploadedFile() file: Express.Multer.File) {
-    return file;
+    if (!file) {
+      throw new BadRequestException('Make sure that the file is an image');
+    }
+    return { file: file.originalname };
   }
 }
